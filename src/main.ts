46b22aca3ff604572acf46ed514820d4919d280e
TatchNicolas <TatchNicolas@users.noreply.github.com>
2021-06-08 11:11:35 +0900

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,16 @@
 import { App, subtype } from "@slack/bolt";
 
-// SocketModeで、URLいらず
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   appToken: process.env.SLACK_APP_TOKEN,
   socketMode: true,
 });
 
-app.message("ayataka", async ({ message, say }) => {
-  // say() sends a message to the channel where the event was triggered
-  console.log(message);
-  await say(`選ばれた`);
-});
-
-// Event typeはApp設定でSubscribeを設定する必要がある
+// Make sure app subscribes events you want to catch in Event Subscription page of your app setting
 app.event("app_mention", async ({ event, client }) => {
   console.log(event);
 });
 
-// 即時関数ってやつ？
 (async () => {
   // Start your app
   await app.start(Number(process.env.PORT) || 3000);
